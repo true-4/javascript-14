76165e92c5e01e481f6c26490407202411733d8f
kuga <esixterb58@gmail.com>
2025-06-26 11:44:02 +0300

    fix: task-1 link

--- index.js
+++ index.js
@@ -7,9 +7,19 @@
 // на https://jsonplaceholder.typicode.com/todos
 //  Выведи в консоль количество пользователей и количество задач, как один объект { usersCount, todosCount }, используя Promise.all.
 Promise.all([ // запускает все запросы одновременно
-  fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()), // fetch возвращает промис, который после then превращается в массив данных
-  fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json())
-]).then(([user, todos]) => { // получаем оба массива
-  const result = {userId: user.length, todosId: todos.length,} // создаём объект с количеством каждого элемента
-  console.log(result)
+  fetch('https://dummyjson.com/users').then(res => res.json()), // fetch возвращает промис, который после then превращается в массив данных
+  fetch('https://dummyjson.com/todos').then(res => res.json())
+]).then(([usersData, todosData]) => {
+  const users = usersData.users;
+  const todos = todosData.todos;
+  const result = {
+    usersCount: users.length,
+    todosCount: todos.length
+  };
+  console.log(result);
 })
+
+// ✅ Задача 2: Проверка скорости API
+// Условие:
+//  Запусти одновременно три запроса на один и тот же URL https://jsonplaceholder.typicode.com/posts/{id} с разными id. Выведи в консоль общее время выполнения и массив постов.
+
